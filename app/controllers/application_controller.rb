2d969356d8b80b6a537ae7cebae1368f62c645c7
Denise McCoy <Denise.L.McCoy@gmail.com>
2014-12-04 13:57:50 -0700

    exercisewithTurek

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :logged_in?
+
+  class AccessDenied < StandardError
+
+  end
+
+  def record_not_found
+    render "/public/403", status: 403
+  end
+
   def current_student
     Student.find_by_id(session[:student_id])
   end
 
   helper_method :current_student
+
+  def logged_in?
+    unless current_student
+      redirect_to login_path
+    end
+  end
 
 end
--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,11 +1,19 @@
 class ClubsController < ApplicationController
 
+  rescue_from AccessDenied, with: :record_not_found
+
   def index
     @clubs = Club.all
   end
 
   def show
     @club = Club.find(params[:id])
+    if @club.students.include? current_student
+    else
+      raise AccessDenied
+    end
   end
+
+
 
 end
